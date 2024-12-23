c851ac38e683b8fe329580add39e612d78dc6d75
tusha-patel <tusha.b@queueloopsolutions.com>
2024-12-23 14:19:35 +0530

    post images changes

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -8,7 +8,7 @@ const PostCard = ({ $id, title, featuredImage, }) => {
             <Link to={`/post/${$id}`} >
                 <div className="w-full bg-gray-100 rounded-xl p-4">
                     <div className="w-full justify-center mb-4 ">
-                        <img src={appWriteServices.getFilePreview(featuredImage)} alt={title} className='rounded-xl' />
+                        <img src={appWriteServices.getFilePreview(featuredImage)} alt={title} className='rounded-xl aspect-square object-cover  ' />
                     </div>
                     <h2 className='text-xl font-bold '> {title} </h2>
                 </div>
